2b8c0e290ce0a982376193bc48a0a52b95338a01
saeyeong <syeongppp@gmail.com>
2020-06-01 20:17:31 +0900

    [ADD] 토론장 페이지 북마크 아이콘 추가

--- app/src/main/java/com/sae/coloseum/view/PostActivity.kt
+++ app/src/main/java/com/sae/coloseum/view/PostActivity.kt
@@ -2,6 +2,7 @@ package com.sae.coloseum.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.sae.coloseum.R
@@ -10,12 +11,13 @@ import com.sae.coloseum.databinding.ActivityPostBinding
 import com.sae.coloseum.model.DataModel
 import kotlinx.android.synthetic.main.activity_post.*
 
-class PostActivity : AppCompatActivity() {
+class PostActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityPostBinding
 
     var model: DataModel? = null
     var adapter: CmtListAdapter? = null
+    var tag: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,11 +27,27 @@ class PostActivity : AppCompatActivity() {
         init()
     }
 
+    override fun onClick(v: View?) {
+        if(tag == "off") {
+            binding.bookmark.setImageResource(R.drawable.bookmark_on)
+            tag = "on"
+        } else {
+            binding.bookmark.setImageResource(R.drawable.bookmark_off)
+            tag = "off"
+        }
+    }
+
     fun init() {
+        setListener()
         model = DataModel()
         adapter = CmtListAdapter(model?.itemsList2)
+        tag = "off"
 
         cmt_list.adapter = adapter
         cmt_list.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun setListener(){
+        binding.bookmark.setOnClickListener(this)
     }
 }
